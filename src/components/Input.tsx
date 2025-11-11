8490d1c3310605a9688133cf9ae16274200fe41f
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-11-11 18:07:55 -0300

    Criando um componente de Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,37 @@
+import React, { forwardRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <div className="relative group flex items-center flex-1 font-semibold transition-all duration-300 ease-in-out   rounded-sm   ">
+      <input
+        ref={ref}
+        {...props}
+        type={props.type === "password" && showPassword ? "text" : props.type}
+        className="flex-1 min-h-[50px] px-2 pr-10 placeholder:text-green-800 rounded-sm border  border-green-900 hover:bg-green-900 hover:text-gray-300 hover:border-green-950 hover:placeholder:text-gray-300  text-green-900 bg-gray-50 transition-all duration-300 ease-in-out "
+      />
+      <AnimatePresence mode="wait">
+        {props.type === "password" && (
+          <motion.img
+            key={showPassword ? "eye-off" : "eye"}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ duration: 0.1 }}
+            src={showPassword ? "eye-off.svg" : "eye.svg"}
+            onClick={() => setShowPassword(!showPassword)}
+            alt="Mostrar/Esconder senha"
+            className="absolute right-2 top-1/2 -translate-y-1/2 group-hover:invert group-hover:grayscale-100 cursor-pointer"
+          />
+        )}
+      </AnimatePresence>
+    </div>
+  );
+});
+
+export default Input;
