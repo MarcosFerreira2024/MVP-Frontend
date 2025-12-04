72a74e09a493e97cf1d902d37812ff8953429423
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-12-04 16:37:04 -0300

    Update Label.tsx

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 type LabelProps = {
   to: string;
   text: string;
+  className?: string;
   children: React.ReactNode;
 };
 
-function Label({ to, text, children }: LabelProps) {
+function Label({ to, text, className, children }: LabelProps) {
   return (
     <label
       htmlFor={to}
-      className="text-gray-50 group relative duration-300 ease-in-out transition-all  flex flex-col gap-2 min-w-full font-segoe font-semibold"
+      className={twMerge(
+        `text-secundary group relative duration-300 ease-in-out transition-all  flex flex-col gap-2 w-full`,
+        className
+      )}
     >
       {text}:{children}
     </label>
