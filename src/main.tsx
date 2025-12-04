4fd90801310adabdd539ba63401759daa8043ecf
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-12-04 16:37:31 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { OutingsProvider } from "./context/OutingsContext.tsx"; // Import OutingsProvider
+import { LoadingProvider } from "./context/LoadingContext.tsx";
+import Loading from "./components/Loading.tsx";
+import { UserProvider } from "./context/UserContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LoadingProvider>
+      <UserProvider>
+        <Loading />
+
+        <OutingsProvider>
+          <App />
+        </OutingsProvider>
+      </UserProvider>
+    </LoadingProvider>
   </StrictMode>
 );
