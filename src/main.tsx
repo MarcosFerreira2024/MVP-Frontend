19c6b5e7120ed3ace208c3826459f850c6686b97
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-06-19 16:16:40 -0300

    chore: remove dead code and unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { OutingsProvider } from "./context/OutingsContext.tsx"; // Import OutingsProvider
+import { OutingsProvider } from "./context/OutingsContext.tsx";
 import { LoadingProvider } from "./context/LoadingContext.tsx";
-import Loading from "./components/Loading.tsx";
 import { UserProvider } from "./context/UserContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LoadingProvider>
       <UserProvider>
-        <Loading />
-
         <OutingsProvider>
           <App />
         </OutingsProvider>
--- src/pages/Testes.tsx
+++
@@ -1,9 +0,0 @@
-import { useUser } from "../context/UserContext";
-
-function Testes() {
-  const { loading } = useUser();
-
-  return <>{loading ? null : null}</>;
-}
-
-export default Testes;
